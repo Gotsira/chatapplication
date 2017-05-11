3019b016121fa09dbe51ccd6cb738eba579c62a6
Issaree Srisomboon <issaree.sr@ku.th>
2017-05-12 01:25:49 +0700

    modified controller classes

--- src/application/HomeController.java
+++ src/application/HomeController.java
@@ -1,5 +1,16 @@
 package application;
 
-public class HomeController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class HomeController extends StageChanged {
+	
+	@FXML
+	private Button chatRoomButton;
+	
+	@FXML
+	public void chatRoomAccess( ActionEvent event ) {
+		setStage("/application/ChatRoom.fxml", "Messenger Chat Room", "home.css", chatRoomButton);
+	}
 }
