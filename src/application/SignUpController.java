b5dae690b4493f5a06e186bc519d8edaee3b9369
Issaree Srisomboon <issaree.sr@ku.th>
2017-05-12 01:06:41 +0700

    create controller class for each fxml file

---
+++ src/application/SignUpController.java
@@ -0,0 +1,54 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import users.Register;
+
+public class SignUpController extends StageChanged {
+	
+	@FXML
+	private Label regisStatus;
+	
+	@FXML
+	private TextField regisUsername;
+	
+	@FXML 
+	private PasswordField regisPassword;
+	
+	@FXML
+	private Button regisButton;
+	
+	@FXML
+	private Button signinButton;
+	
+	@FXML
+	public void signup( ActionEvent event ) throws Exception {
+		Register regis = new Register( getRegisUsername() , getRegisPassword() );
+		if ( getRegisUsername().isEmpty() || getRegisPassword().isEmpty() ) {
+			regisStatus.setText( "Cannot register with an empty username or password.");
+		} else {
+			regis.add();
+			login(event);
+		}
+	}
+	
+	@FXML
+	public void login( ActionEvent event ) {
+		setStage("/application/Login.fxml", "Messenger Login" , "login.css" , regisButton);
+	}
+	
+	public String getRegisUsername() {
+		return regisUsername.getText();
+	}
+	
+	public String getRegisPassword() {
+		return regisPassword.getText();
+	}
+
+}
