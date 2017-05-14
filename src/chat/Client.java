77158ec02695d0119a40cd04f2ae17f92f613e09
Sirasath Piyapootinun <gotmadagascar@gmail.com>
2017-05-14 17:56:23 +0700

    modified server and client

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,27 +1,22 @@
 package chat;
 
+import java.io.IOException;
 
 import com.lloseng.ocsf.client.AbstractClient;
 
 public class Client extends AbstractClient {
 	private String host;
-	private final int PORT = 5555;
+	private static final int PORT = 5555;
 	private String[] message;
 	private String username;
-	
+
 	public Client(String host, int port) {
 		super(host, port);
 	}
 
 	@Override
 	protected void handleMessageFromServer(Object msg) {
-		String chat = (String) msg;
-		if(msg.getClass() == String.class) {
-			message = chat.split(" ");
-			if(message[message.length - 1] == username) {
-				
-			}
-		}
+		
 	}
 
 }
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
+
 import com.lloseng.ocsf.server.AbstractServer;
 import com.lloseng.ocsf.server.ConnectionToClient;
 
@@ -22,45 +24,64 @@ public class Server extends AbstractServer {
 
 	@Override
 	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
-		if(msg.getClass() == String.class) {
-			this.sendToAllClients(msg);
+		String[] message = ((String) msg).split(":");
+		String type = message[0];
+		if (type.equals("connect")) {
+			String name = message[1];
+			client.setInfo("name", name);
+		} else if (type.equals("message")) {
+			String name = message[1];
+			String data = message[2];
+			for (Thread t : getClientConnections()) {
+				ConnectionToClient c = (ConnectionToClient) t;
+				if (c.getInfo("name").equals(name)) {
+					try {
+						c.sendToClient(client.getInfo("name") + ": " + data);
+					} catch (IOException e) {
+						// do nothing
+					}
+				}
+			}
+		} else if (msg.getClass() == byte[].class) {
+			
 		}
 	}
+
 	/**
 	 * 
 	 */
-	public void clientConnected(ConnectionToClient client){
-		System.out.println("The client "+client+" has connected.");
+	public void clientConnected(ConnectionToClient client) {
+		System.out.println("The client " + client + " has connected.");
+
 	}
-	  
+
 	/**
 	 * 
 	 */
-	public void clientDisconnected(ConnectionToClient client){
-		System.out.println("The client "+client +" has disconnected.");
-		//users.remove(client.getInfo("loginID")) ;
+	public void clientDisconnected(ConnectionToClient client) {
+		System.out.println("The client " + client + " has disconnected.");
+		// users.remove(client.getInfo("loginID")) ;
 	}
-	
+
 	/**
 	 * This method overrides the one in the superclass. Called when the server
 	 * starts listening for connections.
 	 */
 	protected void serverStarted() {
-		System.out.println("Server listening for connections on port "
-				+ getPort());
+		System.out.println("Server listening for connections on port " + getPort());
 	}
 
 	/**
-	 * Checks if a user is on the list of current valid users.  Persists even
-	 * if a user logs off
+	 * Checks if a user is on the list of current valid users. Persists even if
+	 * a user logs off
 	 * 
 	 * @param user
 	 * @return
 	 */
-	public boolean doesUserExist(String user){
+	public boolean doesUserExist(String user) {
 		return (users.contains(user));
 	}
-	
+
 	/**
 	 * This method overrides the one in the superclass. Called when the server
 	 * stops listening for connections.
@@ -79,24 +100,23 @@ public class Server extends AbstractServer {
 	 *            [0] The port number to listen on. Defaults to 5555 if no
 	 *            argument is entered.
 	 */
-	
-	 public static void main(String[] args) { 
-		 int port = 0; //Port to listen on
 
-		 try { 
-			 port = Integer.parseInt(args[0]); //Get port from command line 
-		 }
-		 catch(Throwable t) { 
-			 port = DEFAULT_PORT; //Set port to 5555
-		 }
-	 
-		 Server sv = new Server(port);
-		 ServerConsole server = new ServerConsole(sv);
-		 try { 
-			 sv.listen(); //Start listening for connections 
-		 } catch (Exception ex) { 
-			 System.out.println("ERROR - Could not listen for clients!");
-		 }
-		 server.accept();
-	 }
+	public static void main(String[] args) {
+		int port = 0; // Port to listen on
+
+		try {
+			port = Integer.parseInt(args[0]); // Get port from command line
+		} catch (Throwable t) {
+			port = DEFAULT_PORT; // Set port to 5555
+		}
+
+		Server sv = new Server(port);
+		ServerConsole server = new ServerConsole(sv);
+		try {
+			sv.listen(); // Start listening for connections
+		} catch (Exception ex) {
+			System.out.println("ERROR - Could not listen for clients!");
+		}
+		server.accept();
+	}
 }
