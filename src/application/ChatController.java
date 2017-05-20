7b2009172b3f064f226c117ff85854aeb8552d4a
Issaree Srisomboon <issaree.sr@ku.th>
2017-05-20 18:50:36 +0700

    add show message textarea

--- src/application/ChatController.java
+++ src/application/ChatController.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import chat.Client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ChatController extends StageChanged {
@@ -29,6 +31,12 @@ public class ChatController extends StageChanged {
 	
 	@FXML
 	private MenuItem sendContact;
+	
+	@FXML
+	private Label name;
+	
+	@FXML
+	private TextArea message;
 	
 	@FXML
 	public void send(ActionEvent event) throws IOException {
